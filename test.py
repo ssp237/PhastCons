7d950bd89837aadae4ef0a5f42dd6ce65fca308f
ssp237 <ssp237@cornell.edu>
2020-12-20 01:15:51 -0500

    (incorrect) felsenstein

--- test.py
+++ test.py
@@ -1,8 +1,33 @@
 """ Basic test cases for the files """
-from tree import Node
+from tree import Node, read_data
+import numpy as np
 
 s1 = "(((One:0.200000,Two:0.300000):0.300000,(Three:0.500000," \
      "Four:0.300000):0.200000):0.300000,Five:0.700000):0.000000;"
 t1 = Node.from_str(s1)
-print(str(t1))
 assert(str(t1).replace(" ", "") == s1)
+
+
+top1 = "((human:0.075170, dog:0.117610):0.000000, (mouse:0.030590, rat:0.031610):0.142890):0.000000;"
+t1 = Node.from_str(top1)
+assert(str(t1) == top1)
+data = read_data("apoe.fa")
+
+# branch_lengths = np.array(
+#     [[0.07517, 0.03059, 0.03161, 0.11761, 0.14289],
+#      [0.20843, 0.03397, 0.03497, 0.24952, 0.00000],
+#      [0.20843, 0.03397, 0.03497, 0.24952, 0.00000]], dtype=float)
+#
+# index = 0
+# names = ['human', 'mouse', 'rat', 'dog']
+# branches = [0, 1, 2, 3]
+# leaves = [Node(s, None, None, bl, i) for (s, i, bl) in
+#           zip(names, branches, branch_lengths[index, :])]
+# branch_probs = [np.zeros((4, 4), dtype=float) for _ in range(6)]
+# # Note that branch 5 (or 6 in 1-index) is the branch of 0-length
+# if index == 0:
+#     hum_dog = Node(None, leaves[0], leaves[3], 0, 5)
+#     mouse_rat = Node(None, leaves[1], leaves[2], branch_lengths[index, 4], 4)
+#     root = Node('root', hum_dog, mouse_rat, None, None)
+#     ordering = [leaves[0], leaves[3], hum_dog, leaves[1], leaves[2],
+#                 mouse_rat, root]
--- tree.py
+++ tree.py
@@ -2,26 +2,65 @@
 File to store data on phylogenetic trees
 """
 import re
+import numpy as np
+
+
+def read_data(filename):
+    """ Reads data from ```filename``` in fasta format.
+hum_dog = Node(None, leaves[0], leaves[3], 0, 5)
+mouse_rat = Node(None, leaves[1], leaves[2], branch_lengths[index,4], 4)
+root = Node('root', hum_dog, mouse_rat, None, None)
+ordering = [leaves[0], leaves[3], hum_dog, leaves[1], leaves[2], mouse_rat, root]
+    Arguments:
+        filename: name of fasta file to read
+    Returns:
+        sequences: dictionary of outputs (string (sequence id) -> sequence (string))
+        size: length of each sequence
+    """
+    with open(filename, "r") as f:
+        lines = f.readlines()
+        sequences = {}
+        output = ""
+        size = 0
+        curr = ""
+        flag = False
+        for l in lines:
+            if l[0] == ">":
+                if len(output) != 0:
+                    sequences[curr] = output
+                    size = len(output)
+                    output = ""
+                curr = l[2:].strip()
+            else:
+                output += l.strip()
+        sequences[curr] = output
+    return sequences, size
 
 
 class Node:
-    def __init__(self, name, left, right, parent_len=0.0, this_id=0, parent_id=0):
+    def __init__(self, name, left, right, branch_len=0.0, this_id=0, parent_id=0):
         """ Initializes a node with given parameters.
             Arguments:
                 name: name of node (only relevant for leaves)
                 left: left child (Node)
                 right: right child (Node)
-                parent_len: length to parent
+                branch_len: length to parent
         """
+        bases = 'ACGT'
         self.name = name
         self.left = left
         self.right = right
-        self.branch_length = parent_len
+        self.branch_length = branch_len if branch_len else 0.0
         self.id = this_id
         self.parent_id = parent_id
+        self.probs = [None for _ in bases]
+        self.bp = np.array([[Node.jcm(b, a, self.branch_length) for b in bases] for a in bases])
 
     def is_leaf(self):
         return (self.left is None) and (self.right is None)
+
+    def __getitem__(self, item):
+        return self.probs[item]
 
     def swap_names(self, mapping):
         """
@@ -38,11 +77,18 @@ class Node:
         """
         :return: Newick representation of this tree
         """
-        if self.left is None or self.right is None:
-            return f"{self.name}:{self.branch_length:.6f}"
-        elif self.branch_length == 0:  # Root node
-            return f"({self.left}, {self.right}):{self.branch_length:.6f};"
-        return f"({self.left}, {self.right}):{self.branch_length:.6f}"
+        out = ""
+        if self.is_leaf():
+            out = f"{self.name}:{self.branch_length:.6f}"
+        elif self.right is None:
+            out = f"({self.left}){self.name}:{self.branch_length:.6f}"
+        elif self.left is None:
+            out = f"({self.right}){self.name}:{self.branch_length:.6f}"
+        else:
+            out = f"({self.left}, {self.right}):{self.branch_length:.6f}"
+        if self.parent_id == -1:  # Root node
+            out += ";"
+        return out
 
     @classmethod
     def from_str(cls, string):
@@ -53,17 +99,17 @@ class Node:
         """
         tokens = re.findall(r"([^:;,()\s]*)(?:\s*:\s*([\d.]+)\s*)?([,);])|(\S)", string)
 
-        def help(nextid=0, parentid=-1):  # one node
+        def helper(nextid=0, parentid=-1):  # one node
             thisid = nextid
             children = []
 
             name, length, delim, ch = tokens.pop(0)
             if ch == "(":
                 while ch in "(,":
-                    node, ch, nextid = help(nextid + 1, thisid)
+                    node, ch, nextid = helper(nextid + 1, thisid)
                     children.append(node)
                 name, length, delim, ch = tokens.pop(0)
-            length = float(length)
+            length = float(length) if length else 0.0
             if not children:
                 out = Node(name, None, None, length, thisid, parentid)
             elif len(children) == 1:
@@ -74,12 +120,56 @@ class Node:
                 raise TypeError("Passed a non-binary tree to from_str!")
             return out, delim, nextid
 
-        return help()[0]
+        return helper()[0]
 
-    def felsenstein(self, seq):
+    @classmethod
+    def jcm(cls, b, a, t, u=1.0):
+        """ Evaluates P(b|a, t) under the Jukes-Cantor model
+
+        Arguments:
+            b: descendant base (string)
+            a: ancestral base (string)
+            t: branch length (float)
+            u: mutation rate (float, defaults to 1)
+        Returns:
+            prob: float probability P(b|a, t)
+        """
+        e_4ut = np.exp(-4 * u * t / 3)
+        if a == b:
+            return 0.25 * (1 + 3 * e_4ut)
+        else:
+            return 0.25 * (1 - e_4ut)
+
+    def fel_helper(self, ind, data):
+        bases = 'ACGT'
+        if self.is_leaf():
+            c = data[self.name][ind]
+            self.probs = [1 if c == a else 0 for a in bases]
+            return
+
+        if self.left:
+            self.left.fel_helper(ind, data)
+        if self.right:
+            self.right.fel_helper(ind, data)
+
+        for i_a, a in enumerate(bases):
+            p_i, p_j = 0, 0
+            for i_b, b in enumerate(bases):
+                p_i += (self.left[i_b] * self.left.bp[i_a, i_b])
+            for i_c, c in enumerate(bases):
+                p_j += (self.right.probs[i_c] * self.right.bp[i_a, i_c])
+            self.probs[i_a] = p_i * p_j
+
+    def felsenstein(self, data, seqlen):
         """
         Calculate felsenstein probability of this tree
-        :param seq: Sequence? maybe?
+        :param seqlen:
+        :param data: Sequence? maybe?
         :return:
         """
-        pass
+        prob = 0
+        for i in range(seqlen):
+            self.fel_helper(i, data)
+            prob += np.log(0.25 * np.sum(self.probs))
+
+        return prob
